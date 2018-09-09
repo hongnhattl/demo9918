5ba01562ffb136cb6b43ea4d2cb65e9eb74eea76
hongnhattl <txhnhat@gmail.com>
2018-09-09 11:08:29 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{}}>
+          THAI HONG TUYEN
+        </Text> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c13e0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
